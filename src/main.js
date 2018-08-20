303c658091bf80e1c4957d94b6b1141716aa968d
babyplus <maxcrazy@yeah.net>
2018-08-21 07:07:16 +0800

    v-bind

--- src/main.js
+++ src/main.js
@@ -7,31 +7,40 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#origin_app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+//
+// new Vue({
+//   el: '#test_for',
+//   data: {
+//     foodList: [
+//       {
+//         name: '姜',
+//         price: 1,
+//         discount: 0.8
+//       },
+//       {
+//         name: '葱',
+//         price: 2,
+//         discount: 0.9
+//       },
+//       {
+//         name: '蒜',
+//         price: 3,
+//       }
+//     ]
+//   }
+// })
 new Vue({
-  el: '#origin_app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
-
-new Vue({
-  el: '#test_for',
+  el: '#test_bind',
   data: {
-    foodList: [
-      {
-        name: '姜',
-        price: 1,
-        discount: 0.8
-      },
-      {
-        name: '葱',
-        price: 2,
-        discount: 0.9
-      },
-      {
-        name: '蒜',
-        price: 3,
-      }
-    ]
+    url: 'http://www.baidu.com',
+    v_img: 'https://www.baidu.com/img/bd_logo1.png',
+    v_class: 'btn btn-default',
+    isActive: true
   }
 })
